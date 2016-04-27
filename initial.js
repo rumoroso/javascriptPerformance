ed347e35c8dbbc8d01a8641b0d461ba260fcf01f
Jorge Rumoroso <rumoroso@gmail.com>
2016-04-27 21:52:08 +0200

    Update initial.js

--- initial.js
+++ initial.js
@@ -20,15 +20,31 @@
 *        ];
 */
 
-// with this method this line adds a time mark 
+// it adds a time mark in this point of the code 
 // that can be referenced in "measure" method 
-performance.mark('point_A');
+createMark('point_A');
 
-// get a measure between defined points
-performance.measure('measure A to B', 'point_A', 'point_B');
+// it creates a measure between defined marks
+createMeasure('measure A to B', 'point_A', 'point_B');
 
 showPerformanceInfo('measure');
 showPerformanceInfo('mark');
+
+function createMark(markName) {
+    if (performance.mark === undefined) {
+        console.log("performance.mark method not supported");
+        return;
+    }
+    performance.mark(markName);
+}
+
+function createMeasure(measureName, startingPoin, endPoint) {
+    if (performance.measure === undefined) {
+        console.log("performance.measure method not supported");
+        return;
+    }
+    performance.measure(measureName, startingPoin, endPoint);
+}
 
 function showPerformanceInfoByType(type) {
     var performanceEntries = performance.getEntriesByType(type),
